a740d8c191c79e25a3a02a7806249829b04ac4d9
RockRockWhite <1834309151@qq.com>
2021-02-17 16:18:28 +0800

    判断数据库为空 为电影创建数据库

--- Sql.py
+++ Sql.py
@@ -6,3 +6,30 @@ class Sql:
     def __init__(self):
         """初始化数据库"""
         self.db = pymysql.connect(host="localhost", user="root", password="white", database="DoubanSpider")
+
+    def is_table_existed(self, id):
+        """判断该电影的table是否存在"""
+        cursor = self.db.cursor()
+        commond = f"show tables like '{id}'"
+        cursor.execute(commond)
+        data = cursor.fetchone()
+        if data == None:
+            return True
+        else:
+            return False
+
+    def create_table(self, id):
+        """用于为电影创建新的table"""
+        cursor = self.db.cursor()
+        commond = f"create table {id} (name varchar(255))"
+        cursor.execute(commond)
+        self.db.commit()
+
+
+def main():
+    sql = Sql()
+    print(sql.is_table_existed("test4"))
+
+
+if __name__ == '__main__':
+    main()
